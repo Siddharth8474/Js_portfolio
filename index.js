74a608bf63b66acc3280c326c5f55697dd72b535
Siddharth8474 <gujjarakki433@gmail.com>
2025-02-16 14:15:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Hero Animation
+    lottie.loadAnimation({
+      container: document.getElementById('hero-animation'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'https://assets3.lottiefiles.com/packages/lf20_ysud3fbf.json'
+    });
+  
+    // Skill Animations
+    lottie.loadAnimation({
+      container: document.getElementById('html-animation'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'https://assets10.lottiefiles.com/packages/lf20_1zhkdsr4.json'
+    });
+  
+    lottie.loadAnimation({
+      container: document.getElementById('css-animation'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'https://assets9.lottiefiles.com/packages/lf20_XfEKBK.json'
+    });
+  
+    lottie.loadAnimation({
+      container: document.getElementById('js-animation'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'https://assets4.lottiefiles.com/packages/lf20_x1pWB3.json'
+    });
+  
+    lottie.loadAnimation({
+      container: document.getElementById('react-animation'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'https://assets2.lottiefiles.com/packages/lf20_w8b7vntw.json'
+    });
+  
+    // Project Animations
+    lottie.loadAnimation({
+      container: document.getElementById('project-1-animation'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'https://assets8.lottiefiles.com/packages/lf20_glpzzw9t.json'
+    });
+  
+    lottie.loadAnimation({
+      container: document.getElementById('project-2-animation'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'https://assets5.lottiefiles.com/packages/lf20_wx0xtA.json'
+    });
+  });
+  
